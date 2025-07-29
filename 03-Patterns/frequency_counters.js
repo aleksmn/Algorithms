8024bdfde8664674e9bc457f0cbf18846c6cbad9
aleksmn <al-mn@yandex.ru>
2025-07-29 11:07:46 +0300

    anagram problem

--- 03-Patterns/frequency_counters.js
+++ 03-Patterns/frequency_counters.js
@@ -4,20 +4,20 @@
 // Частота значений должна быть одинаковой.
 
 // "Наивное решение"
-// function same(arr1, arr2){
-//     if(arr1.length !== arr2.length){
-//         return false;
-//     }
-//     for(let i = 0; i < arr1.length; i++){
-//         let correctIndex = arr2.indexOf(arr1[i] ** 2)
-//         if(correctIndex === -1) {
-//             return false;
-//         }
-//         console.log(arr2);
-//         arr2.splice(correctIndex,1)
-//     }
-//     return true;
-// }
+function same(arr1, arr2) {
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+    for (let i = 0; i < arr1.length; i++) {
+        let correctIndex = arr2.indexOf(arr1[i] ** 2)
+        if (correctIndex === -1) {
+            return false;
+        }
+        console.log(arr2);
+        arr2.splice(correctIndex, 1)
+    }
+    return true;
+}
 
 // same([1,2,3,2], [9,1,4,4])
 
@@ -49,4 +49,41 @@ function same(arr1, arr2) {
     return true
 }
 
-same([1, 2, 3, 2, 5], [9, 1, 4, 4, 11])
+// same([1, 2, 3, 2, 5], [9, 1, 4, 4, 11])
+
+
+
+// Даны две строки, напишите функцию, чтобы определить, является ли 
+// вторая строка анаграммой первой. Анаграмма — это слово, фраза 
+// или имя, образованное перестановкой букв другого слова, 
+// например, "cinema", образованное из "iceman".
+
+
+function validAnagram(first, second) {
+    if (first.length !== second.length) {
+        return false;
+    }
+
+    const lookup = {};
+
+    for (let i = 0; i < first.length; i++) {
+        let letter = first[i];
+        lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1;
+    }
+    console.log(lookup)
+
+    for (let i = 0; i < second.length; i++) {
+        let letter = second[i];
+        if (!lookup[letter]) {
+            return false;
+        } else {
+            lookup[letter] -= 1;
+        }
+    }
+
+    return true;
+}
+
+// {a: 0, n: 0, g: 0, r: 0, m: 0,s:1}
+// validAnagram('anagrams', 'nagaramm')
+
