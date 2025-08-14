67ef0c1999d7fde337f838c027e34c511f809a98
aleksmn <al-mn@yandex.ru>
2025-08-14 10:51:47 +0300

    add threesum

--- 03-Patterns/pointers.js
+++ 03-Patterns/pointers.js
@@ -5,7 +5,8 @@
 // сумма которых равна нулю, или undefined, если такая пара не существует.
 
 // Примеры использования функции:
-// sumZero([-3, -2, -1, 0, 1, 2, 3]) // [-3, 3]
+// sumZero([-3, -2, -1, 0, 1, 2, 4]) // [-3, 3]
+//              ^             ^
 // sumZero([-2, 0, 1, 3]) // undefined
 // sumZero([1, 2, 3]) // undefined
 
@@ -75,3 +76,42 @@ console.log(countUniqueValues([1,2,2,5,7,7,99]))
 
 
 
+// Напишите функцию под названием threeSum, 
+// которая принимает отсортированный массив целых чисел.
+// Функция должна найти первую тройку, сумма которой равна 0. 
+// Верните массив, который включает оба значения, 
+// сумма которых равна нулю, или undefined, если такая пара не существует.
+
+
+// threeSum([-4, -2,-1, 0, 1, 2, 3, 4])   [-4, 1, 3]
+
+
+function threeSum(arr) {
+    arr.sort((a, b) => a - b); // Сортируем массив
+    const result = [];
+
+    for (let i = 0; i < arr.length - 2; i++) {
+        if (i > 0 && arr[i] === arr[i - 1]) continue; // Пропускаем дубликаты
+
+        let left = i + 1;
+        let right = arr.length - 1;
+
+        while (left < right) {
+            const sum = arr[i] + arr[left] + arr[right];
+            if (sum === 0) {
+                result.push([arr[i], arr[left], arr[right]]);
+                while (left < right && arr[left] === arr[left + 1]) left++; // Пропускаем дубликаты
+                while (left < right && arr[right] === arr[right - 1]) right--; // Пропускаем дубликаты
+                left++;
+                right--;
+            } else if (sum < 0) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+    }
+    return result;
+}
+
+console.log(threeSum([-4, -2, 1, 2, -1, 0, 3, 4]));
