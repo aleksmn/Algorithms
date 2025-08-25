38a52d070930b6c91b68d138cefb6fb48259ab6d
aleksmn <al-mn@yandex.ru>
2025-08-25 10:40:10 +0300

    task solution

--- 04-Recursion/recursion.js
+++ 04-Recursion/recursion.js
@@ -1,7 +1,7 @@
 // Рекурсия - функция, которая вызывает саму себя
-function countDown(num){
+function countDown(num) {
     // условие выхода из рекурсии
-    if(num <= 0) {
+    if (num <= 0) {
         console.log("Пуск!");
         return;
     }
@@ -15,9 +15,9 @@ countDown(3)
 
 
 
-function sumRange(num){
-   if(num === 1) return 1; 
-   return num + sumRange(num-1);
+function sumRange(num) {
+    if (num === 1) return 1;
+    return num + sumRange(num - 1);
 }
 
 console.log(sumRange(7))
@@ -34,9 +34,9 @@ console.log(7 + 6 + 5 + 4 + 3 + 2 + 1)
 
 
 
-function factorial(num){
-    if(num === 1) return 1;
-    return num * factorial(num-1);
+function factorial(num) {
+    if (num === 1) return 1;
+    return num * factorial(num - 1);
 }
 
 console.log(factorial(5))
@@ -44,46 +44,46 @@ console.log(factorial(5))
 
 
 // halper method recursion
-function collectOddValues(arr){
-    
+function collectOddValues(arr) {
+
     let result = [];
 
-    function helper(helperInput){
-        if(helperInput.length === 0) {
+    function helper(helperInput) {
+        if (helperInput.length === 0) {
             return;
         }
-        
-        if(helperInput[0] % 2 !== 0){
+
+        if (helperInput[0] % 2 !== 0) {
             result.push(helperInput[0])
         }
-        
+
         helper(helperInput.slice(1))
     }
-    
+
     helper(arr)
 
     return result;
 }
 
-collectOddValues([1,2,3,4,5,6,7,8,9])
+collectOddValues([1, 2, 3, 4, 5, 6, 7, 8, 9])
 
 
 // pure recursion
-function collectOddValues(arr){
+function collectOddValues(arr) {
     let newArr = [];
-    
-    if(arr.length === 0) {
+
+    if (arr.length === 0) {
         return newArr;
     }
-        
-    if(arr[0] % 2 !== 0){
+
+    if (arr[0] % 2 !== 0) {
         newArr.push(arr[0]);
     }
-        
+
     return newArr.concat(collectOddValues(arr.slice(1)));
 }
 
-console.log(collectOddValues([1,2,3,4,5, 8, 222, 223]))
+console.log(collectOddValues([1, 2, 3, 4, 5, 8, 222, 223]))
 
 
 
@@ -95,3 +95,16 @@ console.log(collectOddValues([1,2,3,4,5, 8, 222, 223]))
 // Примеры
 // flatten([1, 2, 3, [4, 5] ]))         // [1, 2, 3, 4, 5]
 // flatten([1, [2, [3, 4], [[5]]]]))    // [1, 2, 3, 4, 5]
+
+
+function flatten(oldArr) {
+    var newArr = []
+    for (var i = 0; i < oldArr.length; i++) {
+        if (Array.isArray(oldArr[i])) {
+            newArr = newArr.concat(flatten(oldArr[i]))
+        } else {
+            newArr.push(oldArr[i])
+        }
+    }
+    return newArr;
+}
