c227dfce370a8af5f6d9807aebd7e0ac3a85d375
aleksmn <al-mn@yandex.ru>
2025-09-04 17:54:18 +0300

    solution

--- Problems/encode-and-decode-string.js
+++ Problems/encode-and-decode-string.js
@@ -1,2 +1,35 @@
 // Design an algorithm to encode a list of strings to a single string.
 // The encoded string is then decoded back to the original list of strings.
+
+
+function encode(strs) {
+    let res = '';
+    for (let s of strs) {
+        res += s.length + '#' + s;
+    }
+    return res;
+}
+
+
+
+function decode(str) {
+    let res = [];
+    let i = 0;
+    while (i < str.length) {
+        let j = i;
+        while (str[j] !== '#') {
+            j++;
+        }
+        let length = parseInt(str.substring(i, j));
+        i = j + 1;
+        j = i + length;
+        res.push(str.substring(i, j));
+        i = j;
+    }
+    return res;
+}
+
+
+let encoded = encode(['qwrwerweqr', 'wrewerwe', "gjghj"])
+console.log(encoded)
+console.log(decode(encoded))
