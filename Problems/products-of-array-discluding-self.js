bf44b84a25007c560224bcd8c364d41e76882085
aleksmn <al-mn@yandex.ru>
2025-09-04 17:56:22 +0300

    init

---
+++ Problems/products-of-array-discluding-self.js
@@ -0,0 +1,14 @@
+// Given an integer array nums, return an array output where output[i] 
+// is the product of all the elements of nums except nums[i].
+
+// Input: nums = [1,2,4,6]
+
+// Output: [48,24,12,8]
+
+
+
+function productExceptSelf(nums) {
+
+
+
+}
