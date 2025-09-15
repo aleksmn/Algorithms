8198747aff72288ecbbeed3faff757dfc3ba67fc
aleksmn <al-mn@yandex.ru>
2025-09-15 16:10:14 +0300

    solution

--- Problems/01-arrays/longest-consecutive-sequence.js
+++ Problems/01-arrays/longest-consecutive-sequence.js
@@ -8,3 +8,32 @@
 // Вывод: 4
 
 // Пояснение: самая длинная последовательность подряд идущих чисел — [2, 3, 4, 5].
+
+
+function longestConsecutive(nums) {
+    if (nums.length === 0) {
+        return 0;
+    }
+    nums.sort((a, b) => a - b);
+
+    let res = 0,
+        curr = nums[0],
+        streak = 0,
+        i = 0;
+
+    while (i < nums.length) {
+        if (curr !== nums[i]) {
+            curr = nums[i];
+            streak = 0;
+        }
+        while (i < nums.length && nums[i] === curr) {
+            i++;
+        }
+        streak++;
+        curr++;
+        res = Math.max(res, streak);
+    }
+    return res;
+}
+
+console.log(longestConsecutive([2, 20, 4, 10, 3, 4, 5]))
