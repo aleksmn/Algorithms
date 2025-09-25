b5430c11a55c69844bad8c1a3393b4b902b85519
aleksmn <al-mn@yandex.ru>
2025-09-25 17:40:53 +0300

    fix

--- 06-Sorting/insertion-sort.js
+++ 06-Sorting/insertion-sort.js
@@ -1,11 +1,15 @@
-function insertionSort(arr){
-	var currentVal;
-    for(var i = 1; i < arr.length; i++){
-        currentVal = arr[i];
-        for(var k = i - 1; k >= 0 && arr[k] > currentVal; k--) {
-            arr[k+1] = arr[k]
+function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        let key = arr[i]; // Текущий элемент для вставки
+        let j = i - 1;
+
+        // Сдвигаем элементы, которые больше ключа, на одну позицию вперед
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
         }
-        arr[k+1] = currentVal;
+        // Вставляем ключ на его правильное место
+        arr[j + 1] = key;
     }
     return arr;
 }
