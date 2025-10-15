2185ef36363fdbb7202170d1494c197d832edcfa
aleksmn <al-mn@yandex.ru>
2025-10-15 11:55:56 +0300

    init

---
+++ 07-Sorting-2/quick-sort.js
@@ -0,0 +1,44 @@
+
+// 5  2  1  8  4  7  6  3
+
+//             5
+//       3
+// 2  1    4     7  6  8
+
+
+
+
+function pivot(arr, start = 0, end = arr.length - 1) {
+  const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  };
+
+  let pivot = arr[start];
+  let swapIdx = start;
+
+  for (let i = start + 1; i <= end; i++) {
+    if (pivot > arr[i]) {
+      swapIdx++;
+      swap(arr, swapIdx, i);
+    }
+  }
+
+  swap(arr, start, swapIdx);
+  return swapIdx;
+}
+
+// console.log(pivot([100,-3,2,4,6,9,1,2,5,3,23]))
+
+
+function quickSort(arr, left = 0, right = arr.length -1){
+    if(left < right){
+        let pivotIndex = pivot(arr, left, right) //3
+        //left
+        quickSort(arr,left,pivotIndex-1);
+        //right
+        quickSort(arr,pivotIndex+1,right);
+      }
+     return arr;
+} 
+           
+console.log(quickSort([100,-3,2,4,6,9,1,2,5,3,23]))
