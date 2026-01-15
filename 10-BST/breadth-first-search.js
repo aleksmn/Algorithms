4bad99d71c59f97cf103644b74849b9ff8f559c5
aleksmn <al-mn@yandex.ru>
2026-01-15 17:36:04 +0300

    let

--- 10-BST/breadth-first-search.js
+++ 10-BST/breadth-first-search.js
@@ -23,12 +23,12 @@ class BinarySearchTree {
         this.root = null;
     }
     insert(value){
-        var newNode = new Node(value);
+        let newNode = new Node(value);
         if(this.root === null){
             this.root = newNode;
             return this;
         }
-        var current = this.root;
+        let current = this.root;
         while(true){
             if(value === current.value) return undefined;
             if(value < current.value){
@@ -48,7 +48,7 @@ class BinarySearchTree {
     }
     find(value){
         if(this.root === null) return false;
-        var current = this.root,
+        let current = this.root,
             found = false;
         while(current && !found){
             if(value < current.value){
@@ -64,7 +64,7 @@ class BinarySearchTree {
     }
     contains(value){
         if(this.root === null) return false;
-        var current = this.root,
+        let current = this.root,
             found = false;
         while(current && !found){
             if(value < current.value){
@@ -78,7 +78,7 @@ class BinarySearchTree {
         return false;
     }
     BFS(){
-        var node = this.root,
+        let node = this.root,
             data = [],
             queue = [];
         queue.push(node);
@@ -94,7 +94,7 @@ class BinarySearchTree {
 }
 
 
-var tree = new BinarySearchTree();
+let tree = new BinarySearchTree();
 tree.insert(10);
 tree.insert(6);
 tree.insert(15);
